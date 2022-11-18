d11f949803af1a0557515f0f7b1f43096b406891
Nick Caruso <nicaruso312@gmail.com>
2022-11-17 21:52:12 -0500

    Starting some of general files well be using + the nav bar when logged in or not + css changes

---
+++ FinancialSocialNetwork/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FinancialSocialNetwork.Models
+{
+    public class UserModel
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String username { get; set; }
+        public int userID { get; set; }
+        public String email { get; set; }
+    }
+}
