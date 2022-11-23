f6f21f6c214839f5184ec7a3bb50fc5fdb2bc81d
Nick Caruso <nicaruso312@gmail.com>
2022-11-23 16:52:50 -0500

    Start of my ACCOunt page.

---
+++ FinancialSocialNetwork/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialSocialNetwork.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult MyAccount()
+        {
+            ViewBag.isLoggedIn = true;
+            return View();
+        }
+    }
+}
